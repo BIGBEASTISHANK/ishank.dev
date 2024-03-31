b7e7a3a87dd8c2a1219f4b809e9d7d48b99243c6
BIGBEASTISHANK <ishank@bigbeastishank.com>
2024-04-01 04:56:26 +0530

    Color palette design change

--- src/components/ColorPalette.jsx
+++ src/components/ColorPalette.jsx
@@ -14,36 +14,28 @@ export default function ColorPalette() {
       />
 
       {/* Table */}
-      <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 4.1 }}
-        className="overflow-x-auto"
-      >
-        <table className="border border-[#444D7E] w-full">
-          {paletteColors.map((data, index) => (
-            <motion.tr
-              initial={{ y: -50, scale: 0.4, opacity: 0 }}
-              animate={{ y: 0, scale: 1, opacity: 1 }}
-              transition={{ delay: 4.4 + (index * 0.3) / 2 }}
-              key={index}
-            >
-              {/* Color name */}
-              <th className="md:text-xl w-[40%] px-2 border border-[#444D7E]">
-                {data.name}
-              </th>
+      <div className="overflow-x-auto flex flex-col">
+        {paletteColors.map((data, index) => (
+          <motion.div
+            initial={{ y: -50, scale: 0.4, opacity: 0 }}
+            animate={{ y: 0, scale: 1, opacity: 1 }}
+            transition={{ delay: 4.4 + (index * 0.3) / 2 }}
+            key={index}
+            className="flex border-2 border-[#444D7E] rounded-xl my-1 p-2"
+          >
+            {/* Name */}
+            <p className="my-auto pr-3 md:w-[45%] w-[80%] md:font-bold md:text-xl">
+              {data.name}
+            </p>
 
-              {/* Colors */}
-              <th className="px-2 border border-[#444D7E]">
-                <div
-                  className="h-11 px-5 my-2 outline outline-white"
-                  style={{ background: data.hex }}
-                />
-              </th>
-            </motion.tr>
-          ))}
-        </table>
-      </motion.div>
+            {/* Color */}
+            <div
+              className="w-full h-11 my-auto outline outline-white rounded-md"
+              style={{ background: data.hex }}
+            />
+          </motion.div>
+        ))}
+      </div>
     </div>
   );
 }
