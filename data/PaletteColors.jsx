e6bccfef34724c46e722c8c8fa7683053bc4c0ed
BIGBEASTISHANK <ishank@bigbeastishank.com>
2024-04-01 05:31:15 +0530

    Color palette change

--- data/PaletteColors.jsx
+++ data/PaletteColors.jsx
@@ -1,43 +1,46 @@
 export const paletteColors = [
-    {
-      name: "Midnight Black",
-      hex: "#000000",
-    },
-    {
-      name: "Electric Blue",
-      hex: "#33CCFF",
-    },
-    {
-      name: "Slate Gray",
-      hex: "#708090",
-    },
-    {
-      name: "Cobalt Blue",
-      hex: "#0047AB",
-    },
-    {
-      name: "Azure Blue",
-      hex: "#007FFF",
-    },
-    {
-      name: "Ruby Red",
-      hex: "#FF3333",
-    },
-    {
-      name: "Lime Green",
-      hex: "#00FF00",
-    },
-    {
-      name: "Golden Yellow",
-      hex: "#FFFF00",
-    },
-    {
-      name: "Deep Purple",
-      hex: "#9900FF",
-    },
-    {
-      name: "Coral Pink",
-      hex: "#FF69B4",
-    },
-  ];
-  
+  {
+    name: "Midnight Black",
+    hex: "#050607",
+  },
+  {
+    name: "Electric Blue",
+    hex: "#3AB1F5",
+  },
+  {
+    name: "Slate Gray",
+    hex: "#515860",
+  },
+  {
+    name: "Arch White",
+    hex: "#F6F9FC",
+  },
+  {
+    name: "Cobalt Blue",
+    hex: "##0088CC",
+  },
+  {
+    name: "Arch Blue",
+    hex: "#1793D1",
+  },
+  {
+    name: "Ruby Red",
+    hex: "#FF3333",
+  },
+  {
+    name: "Lime Green",
+    hex: "#00FF00",
+  },
+  {
+    name: "Golden Yellow",
+    hex: "#FFFF00",
+  },
+  {
+    name: "Deep Purple",
+    hex: "#9900FF",
+  },
+  {
+    name: "Coral Pink",
+    hex: "#FF69B4",
+  },
+];
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,7 +10,7 @@ export default function Skills() {
       <HeadingBasic
         heading="Skills"
         url="#skills"
-        animationDelay={6.2}
+        animationDelay={6.35}
         description={
           <>
             <p>
@@ -29,7 +29,7 @@ export default function Skills() {
             key={index}
             initial={{ y: 50, scale: 0.4, opacity: 0 }}
             animate={{ y: 0, scale: 1, opacity: 1 }}
-            transition={{ delay: 6.7 + (index * 0.3) / 2 }}
+            transition={{ delay: 6.85 + (index * 0.3) / 2 }}
             className="select-none"
           >
             <div className="bg-[#1E2028] border border-[#444D7E]/20 flex flex-col overflow-auto h-[8rem] md:w-[8rem] w-[6rem] rounded-2xl hover:scale-[1.1] transition-all hover:shadow-xl shadow-md hover:shadow-[#444D7E]/50 shadow-[#444D7E]/50">
