3de185c55e12b435c39520d5e9898082f0632364
BIGBEASTISHANK <ishank@bigbeastishank.com>
2024-04-06 05:12:10 +0530

    Animation update

--- src/components/ColorPalette.jsx
+++ src/components/ColorPalette.jsx
@@ -10,7 +10,7 @@ export default function ColorPalette() {
       <HeadingBasic
         heading="Color Palette"
         url="/#colorPalette"
-        animationDelay={3.8}
+        animationDelay={2.5}
       />
 
       {/* Table */}
@@ -19,7 +19,7 @@ export default function ColorPalette() {
           <motion.div
             initial={{ y: -50, scale: 0.4, opacity: 0 }}
             animate={{ y: 0, scale: 1, opacity: 1 }}
-            transition={{ delay: 4.4 + (index * 0.3) / 2 }}
+            transition={{ delay: 4 + (index * 0.3) / 2 }}
             key={index}
             className="flex border-2 border-[#1793D1] rounded-xl my-1 p-2"
           >
