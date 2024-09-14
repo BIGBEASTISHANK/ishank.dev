7ca1607ded8c2e402ab6a4d8a069d021d6bf0acc
BIGBEASTISHANK <ishank@bigbeastishank.com>
2024-09-15 00:55:32 +0530

    Changed monthly goal

--- data/SponsorData.jsx
+++ data/SponsorData.jsx
@@ -1,4 +1,4 @@
 // Varibale
 export const goalReached = 550;
-export const sponsorGoalAmount = 120000;
+export const sponsorGoalAmount = 15000;
 export const barPercentage = ((goalReached / sponsorGoalAmount) * 100) <= 100 ? ((goalReached / sponsorGoalAmount) * 100) : 100
