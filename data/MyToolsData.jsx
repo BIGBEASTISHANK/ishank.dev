4bef66f8a710ac9a5a58d8474bee94fc358c237a
BIGBEASTISHANK <ishank@bigbeastishank.com>
2024-12-22 21:00:05 +0530

    Click on MyTools card title to opne learning material

--- data/MyToolsData.jsx
+++ data/MyToolsData.jsx
@@ -33,99 +33,123 @@ export const myTools = [
   {
     name: "C++",
     icon: SiCplusplus,
+    learningLink: "https://www.w3schools.com/cpp",
   },
   {
     name: "C#",
     icon: SiCsharp,
+    learningLink: "https://www.w3schools.com/cs",
   },
   {
     name: "C",
     icon: SiC,
+    learningLink: "https://www.w3schools.com/c",
   },
   {
     name: "Python",
     icon: SiPython,
+    learningLink: "https://www.w3schools.com/python",
   },
   {
     name: "HTML",
     icon: SiHtml5,
+    learningLink: "https://www.w3schools.com/html",
   },
   {
     name: "CSS",
     icon: SiCss3,
+    learningLink: "https://www.w3schools.com/css",
   },
   {
     name: "Sass",
     icon: SiSass,
+    learningLink: "https://www.w3schools.com/sass/",
   },
   {
     name: "TailWind CSS",
     icon: SiTailwindcss,
+    learningLink: "https://tailwindcss.com/docs",
   },
   {
     name: "Dart",
     icon: SiDart,
+    learningLink: "https://dart.dev/guides",
   },
   {
     name: "Java Script",
     icon: SiJavascript,
+    learningLink: "https://www.w3schools.com/js",
   },
   {
     name: "Flutter",
     icon: SiFlutter,
+    learningLink: "https://flutter.dev/docs",
   },
   {
     name: "React.js",
     icon: SiReact,
+    learningLink: "https://reactjs.org/docs/getting-started.html",
   },
   {
     name: "Next.js",
     icon: SiNextdotjs,
+    learningLink: "https://nextjs.org/docs",
   },
   {
     name: "Framer Motion",
     icon: TbBrandFramerMotion,
+    learningLink: "https://motion.dev/docs",
   },
   {
     name: "Bash",
     icon: SiGnubash,
+    learningLink: "https://www.javatpoint.com/bash",
   },
   {
     name: "MySQL",
     icon: SiMysql,
+    learningLink: "https://dev.mysql.com/doc/",
   },
 
   // Tools I use
   {
     name: "Linux",
     icon: SiLinux,
+    learningLink: "https://www.javatpoint.com/linux-tutorial",
   },
   {
     name: "VS Codium",
     icon: SiVscodium,
+    learningLink: "https://vscodium.com/",
   },
   {
     name: "NeoVim",
     icon: SiNeovim,
+    learningLink: "https://neovim.io/doc/",
   },
   {
     name: "Unity",
     icon: SiUnity,
+    learningLink: "https://docs.unity.com/",
   },
   {
     name: "Unreal Engine",
     icon: SiUnrealengine,
+    learningLink: "https://docs.unrealengine.com/en-US/Engine/Basics/index.html",
   },
   {
     name: "GitHub",
     icon: SiGithub,
+    learningLink: "https://docs.github.com/en",
   },
   {
     name: "Git",
     icon: SiGit,
+    learningLink: "https://git-scm.com/docs",
   },
   {
     name: "Mongo DB",
     icon: SiMongodb,
+    learningLink: "https://www.mongodb.com/docs",
   },
 ];
--- src/components/(2) Home/MyTools.jsx
+++ src/components/(2) Home/MyTools.jsx
@@ -14,8 +14,9 @@ export default function MyTools() {
         description={
           <>
             <p>
-              My Versatile tools for digital excellence, spanning web development,
-              programming, and creative content.
+              My Versatile tools for digital excellence, spanning web
+              development, programming, and creative content. You can also click
+              on the card title to open learning material.
             </p>
           </>
         }
@@ -29,20 +30,26 @@ export default function MyTools() {
             key={index}
             initial={{ y: 50, scale: 0.4, opacity: 0 }}
             animate={{ y: 0, scale: 1, opacity: 1 }}
-            // transition={{ delay: 3 + (index * 0.3) / 2 }}
+            transition={{ delay: 3 + (index * 0.3) / 2 }}
             className="select-none"
           >
-            <div className="group bg-[#0A0C0E] border border-[#1793D1]/50 hover:border-[#00FF00]/70 flex flex-col overflow-auto h-[8rem] md:w-[8rem] w-[6rem] rounded-2xl hover:scale-[1.1] transition-all hover:shadow-xl shadow-md hover:shadow-[#00FF00]/80 shadow-[#1793D1]/50">
+            <div className="group bg-[#050607] border border-[#1793D1]/50 hover:border-[#00FF00]/70 flex flex-col overflow-auto h-[8rem] md:w-[8rem] w-[6rem] rounded-2xl hover:scale-[1.1] transition-all hover:shadow-xl shadow-md hover:shadow-[#00FF00]/80 shadow-[#1793D1]/50">
               {/* Icons */}
               <div className="p-5">
                 <data.icon className="m-auto md:text-5xl text-4xl" />
               </div>
 
               {/* Divider */}
-              <div className="w-full h-[.05rem] bg-[#1793D1] group-hover:bg-[#00FF00]" />
+              <div className="w-full h-[.05rem] my-[0.013rem] bg-[#1793D1] group-hover:bg-[#00FF00]" />
 
               {/* Name */}
-              <p className="md:text-base text-sm my-auto px-1">{data.name}</p>
+              <a
+                className="md:text-base text-sm px-1 bg-[#1A1E23] h-full content-center" 
+                href={data.learningLink}
+                target="_blank"
+              >
+                {data.name}
+              </a>
             </div>
           </motion.li>
         ))}
